1688707d47499aecddd45d2153d21334b0a2452d
4L3xD <alessandra.olea@gmail.com>
2021-08-25 19:43:24 -0300

    add player movement

---
+++ Physics/Assets/Scripts/Player.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Rigidbody rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        //Destroy(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector3.up * 500);
+        }
+        if(Input.GetKeyDown(KeyCode.D))
+        {
+            rb.AddForce(Vector3.right * 500);
+        }
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            rb.AddForce(Vector3.left * 500);
+        }
+        if(Input.GetKeyDown(KeyCode.W))
+        {
+            rb.AddForce(Vector3.up * 500);
+        }
+    }
+}
